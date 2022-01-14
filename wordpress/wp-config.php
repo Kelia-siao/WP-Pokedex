e1a6d3f336148945e568ac957dc39fddbe4f1253
Kelia-siao <kelia.siao@hetic.net>
2022-01-14 15:21:45 +0100

    Update wp-config.php
    changement nom du site db

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -22,7 +22,7 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'Site_Pokemon' );
+define( 'DB_NAME', 'site_pokemon' );
 
 /** Utilisateur de la base de données MySQL. */
 define( 'DB_USER', 'root' );
